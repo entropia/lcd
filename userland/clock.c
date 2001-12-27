8e7a1d745b5c2b0ea809868ba32c5a1e5905c114
dividuum <dividuum@bl0rg.net>
2001-12-27 22:51:35 +0000

    Uhr repariert CVS ----------------------------------------------------------------------

--- userland/clock.c
+++ userland/clock.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
+#include <time.h>
 #include "output.h"
 
 int main(int argc, char *argv[]) {
-    int i;
-    int strwidth;
     struct tm *tl;
-    
-    hile(1) {
+	time_t t;    
+	int x;
+
+    while(1) {
 	    char buf[1000];
         clearscreen();
-        tl=localtime(time(NULL));
+	t=time(NULL);
+        tl=localtime(&t);
 	sprintf(buf, "%02d:%02d", tl->tm_hour, tl->tm_min);
         putstring(buf, 10,          2,  4, -3);
-	for (x=0, x<60; x++) {
+	for (x=0; x<60; x++) {
 		setpixel(10+x, 55);
 		if (x<tl->tm_sec){
 			setpixel(10+x, 56);
