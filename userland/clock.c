dff3f9fc3e6d72a0e9669f477b85b4613e1da268
tnt <tnt@bl0rg.net>
2001-12-27 23:00:50 +0000

    Bla!

--- userland/clock.c
+++ userland/clock.c
@@ -21,6 +21,10 @@ int main(int argc, char *argv[]) {
 			setpixel(10+x, 56);
 		}
 	}
+	for (x=0, x<20; x++) {
+		setpixel(sin(tl->tm_sec/60.0)*x+50, cos(tl->tm_sec/60.0)*x+33);
+		}
+        }
         writescreen();
         usleep(15000);
     }
--- userland/scroller-tnt.c
+++ userland/scroller-tnt.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
         putstring(argv[1], -i,          8+sin(i/3.0)*8,  4, -5);
         putstring(argv[1], -i*2,          36+sin(i/4.0)*8,  1, 0);
         for (j=0; j < 100; j++) {
-           setpixel(sin(j/(1.0+i))*30+50,cos(j/(1.0+i))*30+33);
+           setpixel(sin(j/(1.0+sin(i/3.0)))*30+50,cos(j/(1.0+cos(i/4.0)))*30+33);
         }
         writescreen();
         usleep(15000);
