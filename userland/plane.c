9fe184750aab73ecded80821b9bc11f642d429a6
dividuum <dividuum@bl0rg.net>
2001-12-27 23:24:04 +0000

    Neuer Schrott

---
+++ userland/plane.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+#include "output.h"
+
+int main(int argc, char *argv[]) {
+    int i=0;
+    int x=0,y=0;
+    while(1) {
+        if (i==0) {
+            x=rand()%XSIZE;
+            y=rand()%YSIZE;
+            clearscreen();
+        }
+        i=(i+1)%1000;
+        while (1) {
+            x+=(XSIZE+1-rand()%2) % XSIZE;
+            y+=(YSIZE+1-rand()%2) % YSIZE;
+            setpixel(x,y);
+            if (rand()%3 == 0)
+                break;
+        }
+        
+        writescreen();
+        usleep(15000);
+    }
+    return 0;
+}
+
+
