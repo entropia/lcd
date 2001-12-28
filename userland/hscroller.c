4c206aa84334f5839c2d02f4648f0b660948caf7
dividuum <dividuum@bl0rg.net>
2001-12-28 12:07:02 +0000

    *** empty log message ***

--- userland/hscroller.c
+++ userland/hscroller.c
@@ -3,17 +3,44 @@
 
 #define MIN(a,b) ((a)<(b)?(a):(b))
 
-void main() {
-    int curline=0;
+char *text;
+int textoffset=0;
+
+int bottomlinepos;
+char line[12];
+
+void neueletztezeile(int charpos) {
+#define CHAR_AT(line, pos) ((pos)>strlen(line)?' ':line[pos])
+    int i;
+    for (i=0; i<11; i++) {
+        line[i] = CHAR_AT(text, charpos+i);
+    }
+    line[11] = 0;
+    bottomlinepos = YSIZE;
+}
+
+void main(int argc, char *argv[]) {
 
     if (argc<2) {
         printf("geh weg");
         exit(-42);
     }
+    
+    neueletztezeile(0);
 
     while(1) {
-        for 
+        int i;
         scrollup(1);
+        bottomlinepos--;
+        if (bottomlinepos<=YSIZE-10) {
+            textoffset+=11;
+            if (textoffset > strlen(text))
+                textoffset = 0;
+            neueletztezeile(textoffset);
+        }
+        for (i=0; i<11; i++) {
+            putcharacter(line[i], i*9, bottomlinepos, 1);
+        }
         writescreen();
         usleep(150000);
     }
