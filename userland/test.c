2d9acec60a95accc6f66fa8488665cc10e3a0fe6
dividuum <dividuum@bl0rg.net>
2001-12-28 12:28:30 +0000

    Sendepause

--- userland/test.c
+++ userland/test.c
@@ -5,10 +5,6 @@ void main() {
     int i=0;
     putstring("Sende",10, 8, 2, 0);
     putstring("pause",10,24, 2, 0);
-    while(1) {
-        scrollup(1);
-        writescreen();
-        usleep(150000);
-    }
+    writescreen();
 }
 
