dbdca9ed6dd52d6364a8d2f3f552110bf226e9ef
dividuum <dividuum@bl0rg.net>
2001-12-29 13:47:20 +0000

    Nichtcheckende (un)setpixel

--- userland/output.h
+++ userland/output.h
@@ -2,13 +2,18 @@
 
 void writescreen();
 void clearscreen();
+
 void setpixel(int x, int y);
-#define putpixel(x, y) setpixel((x), (y))
+#define setpixel_nocheck(x,y) (SCR_AT(x, y)=1)
+
 void unsetpixel(int x, int y);
+#define unsetpixel_nocheck(x,y) (SCR_AT(x, y)=1)
+
+#define putpixel(x, y) setpixel((x), (y))
+
 void putcharacter(char blah, int x, int y, int zoom);
 void putstring(char *string, int x, int y, int zoom, int spacing);
 int  stringwidth(char *string, int zoom, int spacing);
+
 void setblock(int x1, int y1, int x2, int y2);
 void scrollup(int lines);
-
-
